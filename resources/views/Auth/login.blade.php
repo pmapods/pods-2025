99c0dbd99799a82dfb95ee37224cec394044b889
kevinfarel <kevinfarel@gmail.com>
2021-06-24 09:06:27 +0700

    title icon

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -4,6 +4,7 @@
     <title>Pinus Merah Abadi</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/assets/logo.png" type="image/x-icon">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
